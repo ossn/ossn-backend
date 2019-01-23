0be8192802d58ec5217a620702e926d819124a10
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-23 08:55:56 +0100

    fix: Fix wrong json field names

--- models/event.go
+++ models/event.go
@@ -11,12 +11,12 @@ type Event struct {
 	StartDate       *time.Time `json:"startDate"`
 	EndDate         *time.Time `json:"endDate"`
 	Location        *Location  `json:"location" gorm:"foreignkey:LocationID;association_foreignkey:ID"`
-	LocationID      *uint      `json:"location_id"`
+	LocationID      *uint      `json:"locationId"`
 	ImageURL        *string    `json:"imageUrl" sql:"type:text;"`
 	Description     *string    `json:"description" sql:"type:text;"`
 	SortDescription *string    `json:"sortDescription" sql:"type:text;"`
 	Club            *Club      `json:"club" gorm:"foreignkey:ClubID;"`
-	ClubID          *uint      `json:"club_id"`
+	ClubID          *uint      `json:"clubId"`
 	PublishedAt     *time.Time `json:"publishedAt" gorm:"index:event_published_at"`
 }
 
