7db48d20714686dd251f734eee3bc935dd688094
Alexandros Dorodoulis <aldor94@gmail.com>
2019-02-14 16:39:27 +0100

    fix: Fix heroku redis connection

--- models/redis.go
+++ models/redis.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"log"
+	"net/url"
 	"os"
 	"strings"
 
@@ -11,8 +12,19 @@ import (
 var RedisClient *redis.Client
 
 func init() {
-	redisURL := strings.Split(os.Getenv("REDIS_URL"), "redis://")
-	RedisClient = redis.NewClient(&redis.Options{Addr: redisURL[len(redisURL)-1]})
+	redisURL := os.Getenv("REDIS_URL")
+	password := ""
+	if !strings.Contains(redisURL, "localhost") {
+		parsedURL, _ := url.Parse(redisURL)
+		password, _ = parsedURL.User.Password()
+		redisURL = parsedURL.Host
+	}
+
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr:     redisURL,
+		Password: password,
+	})
+
 	err := RedisClient.Ping().Err()
 	if err != nil {
 		log.Fatal(err)
