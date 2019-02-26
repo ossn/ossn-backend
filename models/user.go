48a64b5f66d97c89a5c755c87e12f94560b8fa24
Alexandros Dorodoulis <aldor94@gmail.com>
2019-02-26 17:57:16 +0100

    fix: Cleanup clubUserRole after user deletion

--- models/user.go
+++ models/user.go
@@ -1,10 +1,12 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type User struct {
 	Model
-	Email             string          `json:"email" gorm:"UNIQUE;"`
-	UserName          string          `json:"userName" gorm:"UNIQUE;" sql:"not null"`
-	Name              string          `json:"lastName" sql:"not null"`
+	Email             string          `json:"email" gorm:"UNIQUE;" validate:"required,email"`
+	UserName          string          `json:"userName" gorm:"UNIQUE;" sql:"not null" validate:"required"`
+	Name              string          `json:"name" sql:"not null"`
 	ImageURL          *string         `json:"imageUrl" sql:"type:text;"`
 	ReceiveNewsletter *bool           `json:"receiveNewsletter" gorm:"default:false;"`
 	Description       *string         `json:"description" sql:"type:text;"`
@@ -14,4 +16,8 @@ type User struct {
 	PersonalURL       *string         `json:"personalUrl" sql:"type:text;"`
 	OIDCID            string          `json:"oidcId" gorm:"index:idx_oidc_id;UNIQUE;column:oidc_id"`
 	AccessToken       string          `json:"accessToken" gorm:"not null; index:idx_access_token"`
+}
+
+func (u *User) AfterDelete(tx *gorm.DB) (err error) {
+	return tx.Unscoped().Where("user_id = ?", u.ID).Delete(&ClubUserRole{}).Error
 }
